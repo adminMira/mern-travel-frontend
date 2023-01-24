77e9b59929d791b52e6f564c6af5cfb93f815e1b
Aggin Evals <89240832+BlackManFromAfrica@users.noreply.github.com>
2023-01-24 20:27:16 +0400

    Update AllRoutes.jsx

--- src/pages/AllRoutes.jsx
+++ src/pages/AllRoutes.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import RouteCard from "../components/RouteCard.jsx";
 import Grid from "@mui/material/Grid";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import Container from "@mui/material/Container";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRoutes } from "../redux/slices/route.js";
@@ -16,7 +18,7 @@ const AllRoutes = () => {
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 p-8">
       {(isRoutesLoading ? [...Array()] : routes.items).map((obj, index) =>
         isRoutesLoading ? (
-          <div></div>
+           <CircularProgress />
         ) : (
           <RouteCard
             id={obj._id}
